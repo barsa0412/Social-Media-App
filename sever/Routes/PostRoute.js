82ebfaf37ab4cbac0677e9e22cdb14cb029e8e99
barsa0412 patro <barsapatro0412@gmail.com>
2025-01-13 20:02:34 +0000

    postroutes

---
+++ sever/Routes/PostRoute.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { createPost, deletePost, getPost, like_dislike_Post, timeline, updatePost } from '../Controllers/PostController.js';
+
+const router = express.Router();
+
+router.post('/', createPost);
+router.get('/', getPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.put('/:id/like_dislike', like_dislike_Post);
+router.get('/:id/timeline', timeline);
+
+export default router;
+
+
+
+
