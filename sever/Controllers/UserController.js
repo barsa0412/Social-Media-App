0d62fffd284798aec01d96ced4f6d154f8260793
barsa0412 patro <barsapatro0412@gmail.com>
2025-01-23 19:14:41 +0000

    usercontroller

---
+++ sever/Controllers/UserController.js
@@ -0,0 +1,62 @@
+import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+// grt all users
+export const getAllUsers = async (req, res) => {
+
+    try {
+        let users = await UserModel.find();
+        
+        users = users.map((user) => {
+            const {password, ...otherDetails } = user._doc;
+            return otherDetails;
+        })
+
+        res.status(200).json(users);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+
+//get a user
+export const getUser = async (req, res) =>{
+    const id = req.params.id;
+    
+    try {
+
+        const user = await UserModel.findById(id);
+
+        if (user) {
+            const {password, ...otherDetails } =user._doc
+            res.status(200).json(otherDetails)
+        } else {
+            res.status(404).json("Please, Try again it is invalid user!")
+        }
+    }
+    catch(error) {
+        res.status(500).json(error)
+    }
+}
+
+
+//update a user
+
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+
+    const {_id, password } = req.body;
+
+    if (id === _id) {
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            let pass = password.toString();
+            req.body.password = await bcrypt.hash(pass, salt)
+        }
+    }
+}
