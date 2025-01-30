ea9221fc6aaed43cae85f11eec9cac9c23a44fc0
barsa0412 patro <barsapatro0412@gmail.com>
2025-01-30 13:45:22 +0000

    index

---
+++ sever/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js';
+import UserRoute from './Routes/UserRoute.js';
+import PostRoute from './Routes/PostRoute.js';
+import UploadRoute from './Routes/UploadRoute.js';
+
+
+//Routes
+const app = express();
+
+
+//to serve image for public (public folder)
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
+
+
+//middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+
+mongoose.connect
+    (process.env.MANGO_DB, { useNewUrlParser: true, useUnifiedTopology: true}
+    ). then(() =>
+        app.listen(process.env.PORT, () => console.log(`listening at ${process.env.PORT}`))
+    ).catch((error) =>
+        console.log('error')
+    )
+
+
+    //use of routes
+
+    app.use('/auth', AuthRoute);
+    app.use('/user', UserRoute);
+    app.use('/post', PostRoute);
+    app.use('/upload', UploadRoute);
